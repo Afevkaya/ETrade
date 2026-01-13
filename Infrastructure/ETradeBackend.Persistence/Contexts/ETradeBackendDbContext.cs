24fd86364bf1c37a092960c34d77f6aa9cf47dfa
afevk <afevkaya5151@gmail.com>
2026-01-13 21:20:14 +0300

    Add UpdatedDate property and update SaveChangesAsync logic in BaseEntity

--- Infrastructure/ETradeBackend.Persistence/Contexts/ETradeBackendDbContext.cs
+++ Infrastructure/ETradeBackend.Persistence/Contexts/ETradeBackendDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using ETradeBackend.Domain.Entities;
+using ETradeBackend.Domain.Entities.Common;
 using Microsoft.EntityFrameworkCore;
 
 namespace ETradeBackend.Persistence.Contexts;
@@ -14,6 +15,29 @@ public class ETradeBackendDbContext : DbContext
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ETradeBackendDbContext).Assembly);
         base.OnModelCreating(modelBuilder);
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var entries = ChangeTracker.Entries<BaseEntity>();
+        var utcNow = DateTime.UtcNow;
+
+        foreach (var entry in entries)
+        {
+            switch (entry.State)
+            {
+                case EntityState.Added:
+                    entry.Entity.CreatedDate = utcNow;
+                    entry.Property(nameof(BaseEntity.UpdatedDate)).IsModified = false;
+                    break;
+                case EntityState.Modified:
+                    entry.Entity.UpdatedDate = utcNow;
+                    entry.Property(nameof(BaseEntity.CreatedDate)).IsModified = false;
+                    break;
+            }
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
     }
 
     public DbSet<Product> Products { get; set; }
---
+++ Infrastructure/ETradeBackend.Persistence/Migrations/20260113153355_mig_2_AddUpdatedDate.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ETradeBackend.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_2_AddUpdatedDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "products",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "orders",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "customers",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "products");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "orders");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "customers");
+        }
+    }
+}
